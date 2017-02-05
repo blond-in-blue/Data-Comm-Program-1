e80e37c55058dca39d819b41e6e17adb01c888eb
Blond-in-Blue <hholder@me.com>
2017-02-04 23:06:19 -0600

    Initial Commit

---
+++ Data Comm Programming Assignment 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Comm Programming Assignment 1
+//
+//  Created by Hunter Holder on 2/4/17.
+//  Copyright © 2017 Hunter Holder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
