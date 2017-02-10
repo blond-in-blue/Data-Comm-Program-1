8f0fff28df476685ff34bb7d0b8bd8cbe36dd233
Blond-in-Blue <hholder@me.com>
2017-02-09 21:42:46 -0600

    Add test.txt  and makefile

--- Data Comm Programming Assignment 1/server.cpp
+++ Data Comm Programming Assignment 1/server.cpp
@@ -19,10 +19,43 @@
 
 using namespace std;
 
+#define BUFFERMAX 4096
+#define CLIENTMAX 1
 
+int accept_conn(int sock)
+{
+	int dataconnfd;
+	socklen_t dataclilen;
+	struct sockaddr_in datacliaddr;
+	
+	dataclilen = sizeof(datacliaddr);
+	//accept a connection
+	if ((dataconnfd = accept (sock, (struct sockaddr *) &datacliaddr, &dataclilen)) <0) {
+		cerr<<"Problem in accepting the data socket"<<endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	return(dataconnfd);
+}
 
+// argv[1]: Port
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World\n";
+	
+	int mysocket = 0;  mysocket=socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in server;
+	memset((char *) &server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(argv[1]));
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	// Get random port number
+	int randomPort = 0;
+	
+	do {
+		randomPort = rand();
+	} while (randomPort <= 1024 && randomPort >= 65535);
+	
+	cout << randomPort;
+
     return 0;
 }
